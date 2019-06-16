d2f955c0c6765c7219714e1dbd333d57e96f5bc8
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2019-06-16 19:52:12 -0400

    Messed up diamond fill

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var gridSize=50;
 
 // var districtColors=["#FA8072","#228B22","#87CEEB","#FF00FF","#FF1493","	#2F4F4F","#FFDEAD"]
 
-var districtColors=["#DC143C","#FF4500","#FFFF00","#00FF00","#87CEFA","#FF00FF","#00FFFF"]
+var districtColors=["#000000","#DC143C","#FF4500","#FFFF00","#00FF00","#87CEFA","#FF00FF","#00FFFF"]
 var graphicBuffer=[]
 
 var numberOfVoters=100;
@@ -20,7 +20,8 @@ var totalRepublicans=numberOfVoters*(1-democratPercentage);
 var totalDemocrats=numberOfVoters*democratPercentage;
 
 var districtCount=7;
-var polls=[-1,0,0,0,0,0,0,0]
+var districts=[gridWidth*gridHeight,0,0,0,0,0,0,0]
+var polls=    [-1,0,0,0,0,0,0,0]
 
 var dirX=[1,-1,0,0];
 var dirY=[0,0,1,-1];
@@ -40,7 +41,7 @@ function CreateVoters (number,party) {
 
 
 function RandomGridPosition() {
-	return Math.floor(Math.random()*gridWidth);
+	return {x:Math.floor(Math.random()*gridWidth),y:Math.floor(Math.random()*gridHeight)}
 }
 var worm={x:0,y:0,dx:1,dy:0,c:0};
 
@@ -52,14 +53,22 @@ function setup() {
 	CreateVoters(totalRepublicans,1);
 	// worm.x=RandomGridPosition().x;
 	// worm.y=RandomGridPosition().y;
-	FloodDistricts(districtCount);
-	DistrictWinners();
+	//FloodDistricts(districtCount);
+	//DistrictWinners();
+	seed_districts();
+
 	update();
  //ColorPatches()	
 }
 var drawGame=true
 //runs every frame
 function update(timestamp){
+	if (districts[0]!=0) {
+		console.log("ran");
+		diamond_fill();
+	}
+	
+
 	if (drawGame) {
 		drawGame=false;
 		setTimeout(draw,.1)
@@ -69,23 +78,19 @@ function update(timestamp){
 
 //runs at a set time
 function draw() {
-	// console.log("Draw")
 	var frame=grid;
 	if (graphicBuffer.length!=0) {
 		frame=graphicBuffer.shift();
-		// console.log("buffer");
-		// console.log(frame==grid);
 		DrawDistricts(frame);
+		console.log("buffering");
 		drawGame=true;
-
 	}
 	else {
-		DrawWinners();
-		var txt = document.getElementById("win");
-		txt.innerHTML="R "+republicanWins+" to D "+democratWins+". "+(democratWins>republicanWins ? "Democrats" : "Republicans")+" win!"
+		//DrawWinners();
+		//var txt = document.getElementById("win");
+		//txt.innerHTML="R "+republicanWins+" to D "+democratWins+". "+(democratWins>republicanWins ? "Democrats" : "Republicans")+" win!"
 	}
-			DrawVoters(frame);
-
+	DrawVoters(frame);
 }
 
 function DrawDistricts(frame) {
@@ -114,7 +119,6 @@ function DrawVoters(frame) {
 			icon="D"
 			ctx.fillStyle = "#0000FF";
 		}
-		// console.log(v)
 		ctx.fillText(icon, v.x*gridSize, v.y*gridSize);
 	});
 }
@@ -123,7 +127,6 @@ function DistrictWinners() {
 	for (x=0; x<gridWidth; x++) {
 		for (y=0; y<gridHeight; y++) {
 			if (grid[x][y].district!=0) {
-				console.log(grid[x][y].votes);
 				polls[grid[x][y].district]+=grid[x][y].votes;
 			}
 		}
@@ -136,11 +139,9 @@ function DistrictWinners() {
 		else {
 			democratWins++;
 		}
-		console.log("Demo"+democratWins)
 	}
 	for (x=0; x<gridWidth; x++) {
 		for (y=0; y<gridHeight; y++) {
-			// console.log(polls[1]);
 			grid[x][y].winner=polls[grid[x][y].district];
 		}
 	}
@@ -150,8 +151,7 @@ function DrawWinners() {
 	for (x=0; x<gridWidth; x++) {
 		for (y=0; y<gridHeight; y++) {
 			if (grid[x][y].district!=0) {
-				console.log(grid[x][y].winner);
-				ctx.fillStyle= grid[x][y].winner>0 ? "#640000" : "#000064";
+				ctx.fillStyle= grid[x][y].winner>0 ? rgb(grid[x][y].district*32+20,0,0) : rgb(0,0,grid[x][y].district*32+20);
 				ctx.fillRect(x*gridSize,y*gridSize,50,50);
 			}
 
@@ -184,9 +184,7 @@ function RandomWorm() {
 //runs until it hits itself!
 function SnakeWorm() {
 	grid[worm.x][worm.y].district=true;
-	console.log(GetGridProperty(worm.x+worm.dx,worm.y+worm.dy,"district")==true)
 	if (GetGridProperty(worm.x+worm.dx,worm.y+worm.dy,"district")==true) {
-		// console.log("checking");
 		var foundValid=false;
 		for (i=0; i<4; i++) {
 			worm.dx=dirX[i];
@@ -196,8 +194,7 @@ function SnakeWorm() {
 			}
 		}
 		if (!foundValid) {
-			worm.x=RandomGridPosition();
-			worm.y=RandomGridPosition();	
+			worm=RandomGridPosition();
 			worm.c=0;
 		}
 	
@@ -221,7 +218,10 @@ function ColorPatches() {
 
 function GetGridProperty(x,y,property) {
 	if (x<0 || x>=gridHeight || y<0 || y>=gridWidth) return null
-	else return grid[x][y][property]
+	else if (property) {
+		return grid[x][y][property]
+	}
+	return grid[x][y]
 }
 
 function DijstraMap(tx,ty) {
@@ -244,35 +244,27 @@ function DijstraMap(tx,ty) {
 }
 var safetyBreak=0//remove later-just makes sure evil while loops dont run forever!
 function FloodDistricts(num_districts) {
-	console.log("Flood Start")
 	for (var d=0;d<num_districts;d++) {
     	var rx=0;
   		var ry=0;
     	do {
-      		rx=RandomGridPosition();
-      		ry=RandomGridPosition();
+    		r=RandomGridPosition()
       		safetyBreak++;
-      		console.log(safetyBreak);
-    	} while (grid[rx][ry].district!=0 && safetyBreak<1000)
+    	} while (grid[r.x][r.y].district!=0 && safetyBreak<1000)
     	safetyBreak=0;
     	FloodFill(rx,ry,0,d,50)
-    	// FillHoles(d);
-      	// console.log(FloodFill(rx,ry,0,d,50)); 
-      	// FillHoles(d);         
+    	// FillHoles(d);       
   	}
-  	// FloodFill(rx,ry,0,1,50);
-  	// FloodFill(rx,ry,0,2,50);
 }
 
 function FloodFill(posx,posy,target,replace,amount) {
   var pos=GetGridProperty(posx,posy,"district")
-  console.log(amount);
   if (pos == null || pos!=target || amount==0) return amount;
   grid[posx][posy].district=replace;
   amount--;
   GraphicStack();
   var order=[0,1,2,3];
-  order=shuffle(order);
+ /// order=shuffle(order);
   for (var f=0; f<4;f++) {
     amount=FloodFill(posx+dirX[order[f]], posy+dirY[order[f]], target, replace,amount); 
     if (amount==0) return amount;
@@ -301,8 +293,6 @@ function GraphicStack() {
 	// let frame=grid.slice(0);
 	frame=JSON.parse(JSON.stringify(grid));
 	graphicBuffer.push(frame);
-	console.log(frame[0][0].district);
-
 }  
 function GetSig(x,y,type) {
   var sig=0
@@ -316,11 +306,97 @@ function GetSig(x,y,type) {
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
-  	console.log("zzz")
   	DrawDistricts();
     if ((new Date().getTime() - start) > milliseconds){
       break;
     }
   }
+}
+function rgb(r, g, b){
+  return "rgb("+r+","+g+","+b+")";
+}
+function seed_districts() {
+	for (var d=1; d<districtCount+1;d++) {
+		do {
+			r=RandomGridPosition()
+			safetyBreak++
+		} while (grid[r.x][r.y].district!=0 && safetyBreak<10)
+		fill_self(r.x,r.y,0,d)
+	}	
+}
+
+
+function diamond_fill() {
+	// for (var d=1; d<districtCount-1;d++) {
+	let borderDistricts=[];
+	//borderDistricts[0]=[]
+	for (d=1; d<districts.length; d++) {
+		borderDistricts[d-1]={number:d-1,tiles:[]}
+	}
+	//console.log(borderDistricts)
+	for (x=0; x<gridWidth; x++) {
+		for (y=0; y<gridHeight; y++) {
+			var dis=grid[x][y].district;
+			//console.log(dis)
+			if (dis!=0 && GetSig(x,y,dis)<4) {
+				borderDistricts[dis-1].tiles.push({x:x,y:y})
+			}
+		}
+	}
+	do {
+		borderDistricts=borderDistricts.sort(function(a,b){return a.tiles.length-b.tiles.length});
+	 	r=borderDistricts[0].tiles[Math.floor(Math.random()*borderDistricts[0].tiles.length)]
+	 	console.log(borderDistricts);
+	 	if (r==null) return;
+	 	fill_neighbors(r.x,r.y,0,borderDistricts[0].number+1);
+	 	borderDistricts[0].tiles.splice(r,1);
+	 	while (borderDistricts.length!=0 && borderDistricts[0].tiles.length==0) {
+	 		borderDistricts.shift();
+	 	}
+	} while (borderDistricts.length!=0 && safetyBreak<100)
+	//console.log("ended");
+	//console.log(borderDistricts.sort(function(a, b){return a.length-b.length}))
+	//var sort=[1,2,3,4,5,6,7,8]
+	// sort=shuffle(sort);
+	//borderDistricts=shuffle(borderDistricts)
+	// var order=borderDistricts;
+	// do {
+	//  	order=order.sort(function(a, b){return a.length-b.length})
+	//  	if (order[0].length==0) {
+	// 		order.shift();
+	// 		console.log(order);
+	// 	}
+	// 	if (order[0].length!=0) {
+	// 		r=Math.floor(Math.random()*order[0].length)
+	// 		fill_neighbors(order[0][0].x,order[0][0].y,0,d);
+	// 		order[0].splice(r,1);
+	// 	}
+	//     if (order[0].length==0) {
+	// 		order.shift();
+	// 		console.log(order);
+	// 	}
+	// 	safetyBreak++;
+	// } while (order.length!=0 && safetyBreak<100)
+	if (safetyBreak>100) {
+		safetyBreak=0
+		console.log("broke");
+	}
+}
+
+function fill_neighbors(x,y,target,replace) {
+	var order=[0,1,2,3]
+	order=shuffle(order);
+ 	for (var f=0; f<4;f++) {
+    	fill_self(x+dirX[order[f]], y+dirY[order[f]], target, replace); 
+  	}
+}
+function fill_self(x,y,target,replace) {
+
+	var self=GetGridProperty(x,y)
+	if (self==null || self.district==replace) return
+	districts[target]--;
+	districts[replace]++;
+	self.district=replace;
+	GraphicStack();
 }
 setup();
